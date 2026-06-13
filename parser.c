000e3c25f0dcf2152f53bd948c0530a167a615d0
bruhmoment3124 <73769756+bruhmoment3124@users.noreply.github.com>
2026-06-13 03:44:46 -0400

    Parser additions
    Made a number of changes and implemented a few functions, most notably type and fieldlist. More verifcation will have to be done, as well as revisions as the code is quite verbose

--- parser.c
+++ parser.c
@@ -8,18 +8,112 @@
 	Below is the recursive descent parser.
 */
 
-/*
+/* NOT FINISHED */
+void constant(void)
+{
+	prologue("constant");
+	
+	if(match_sym("+") || 
+	   match_sym("-") ||
+	   match_type(identifier) || 
+	   match_type(unum) || 
+	   match_type(uint))
+	{
+		if(match_sym("+") || match_sym("-"))
+			advance_sym();
+			
+		if(match_type(identifier) || 
+		   match_type(unum) || 
+	       match_type(uint))
+		{
+			advance_sym();
+		} else
+		{
+			/* replace with standard syntax error function */
+			printf("ERROR!\n");
+			exit(-1);
+		}
+	} else if(match_type(string))
+	{
+		advance_sym();
+	} else
+	{
+		/* replace with standard syntax error function */
+		printf("ERROR!\n");
+		exit(-1);
+	}
+	
+	epilogue();
+}
 
-Below is the basic parsing of expressions:
+void variable(void)
+{
+	prologue("variable");
+	
+	epilogue();
+}
 
+/* factor will be difficult, because it relies on type information */
 void factor(void)
 {
 	prologue("factor");
 	
-	if(match_type(uint) || match_type(unum))
-		expect_type(get_tok(stay).type_pasc);
-	else
-		printf("ERROR!\n"), exit(-1);
+	/* unsigned constant needs to be implemented */
+	
+	/* some work will have to be done to make LL(1) */
+	if(match_type(identifier))
+	{
+		variable();
+	} else if(match_sym("("))
+	{
+		expect_sym("(");
+		expression();
+		expect_sym(")");
+	} else if(match_sym("not"))
+	{
+		expect_sym("not");
+		factor();
+	} else if(match_sym("["))
+	{
+		expect_sym("[");
+		
+		if(match_sym("]"))
+		{
+			expect_sym("]");
+		} else
+		{
+			expression();
+			
+			if(match_sym(".."))
+			{
+				expect_sym("..");
+				expression();
+			}
+			
+			while(match_sym(","))
+			{
+				expect_sym(",");
+				
+				expression();
+			
+				if(match_sym(".."))
+				{
+					expect_sym("..");
+					expression();
+				}
+			}
+			
+			expect_sym("]");
+		}
+	} else if(match_type(unum) || match_type(uint))
+	{
+		expect_type(get_tok(stay).type);
+	} else
+	{
+		/* replace with standard syntax error function */
+		printf("ERROR!\n");
+		exit(-1);
+	}
 	
 	epilogue();
 }
@@ -84,41 +178,9 @@ void expression(void)
 	epilogue();
 }
 
-*/
-
-void constant(void)
+void write_param_list(void)
 {
-	prologue("constant");
-	
-	if(match_sym("+") || 
-	   match_sym("-") ||
-	   match_type(identifier) || 
-	   match_type(unum) || 
-	   match_type(uint))
-	{
-		if(match_sym("+") || match_sym("-"))
-			advance_sym();
-			
-		if(match_type(identifier) || 
-		   match_type(unum) || 
-	       match_type(uint))
-		{
-			advance_sym();
-		} else
-		{
-			/* replace with standard syntax error function */
-			printf("ERROR!\n");
-			exit(-1);
-		}
-	} else if(match_type(string))
-	{
-		advance_sym();
-	} else
-	{
-		/* replace with standard syntax error function */
-		printf("ERROR!\n");
-		exit(-1);
-	}
+	prologue("write_param_list");
 	
 	epilogue();
 }
@@ -354,12 +416,250 @@ void proc_or_func_head(void)
 	epilogue();
 }
 
-/* UNFINISHED TYPE FUNCTION, WILL COMPLETE LATER */
-void type_pasc(void)
+/* ALMOST DONE, NEED TO ENSURE VERIFY TYPE OF IDENTIFIER */
+void ordinal_type(void)
 {
-	prologue("type_pasc");
+	prologue("ordinal_type");
 	
-	expect_sym("type");
+	if(match_type(identifier))
+	{
+		expect_type(get_tok(stay).type);
+	} else if(match_sym("("))
+	{
+		expect_sym("(");
+		
+		expect_type(identifier);
+		while(match_sym(","))
+		{
+			expect_sym(",");
+			expect_type(identifier);
+		}
+		
+		expect_sym(")");
+	} else
+	{
+		constant();
+		expect_sym("..");
+		constant();
+	}
+	
+	epilogue();
+}
+
+
+void data_type(void)
+{
+	prologue("data_type");
+	
+	if(match_sym("^"))
+	{
+		expect_sym("^");
+		expect_type(identifier);
+	} else if(match_sym("packed") ||
+			  match_sym("array") ||
+			  match_sym("file") ||
+			  match_sym("set") ||
+			  match_sym("record"))
+	{
+		if(match_sym("packed"))
+			expect_sym("packed");
+		
+		if(match_sym("array"))
+		{
+			expect_sym("array");
+			expect_sym("[");
+			
+			ordinal_type();
+			
+			while(match_sym(","))
+			{
+				expect_sym(",");
+				ordinal_type();
+			}
+			
+			expect_sym("]");
+			expect_sym("of");
+			data_type();
+		} else if(match_sym("file"))
+		{
+			expect_sym("file");
+			expect_sym("of");
+			data_type();
+		} else if(match_sym("set"))
+		{
+			expect_sym("file");
+			expect_sym("of");
+			ordinal_type();
+		} else
+		{
+			expect_sym("record");
+			field_list();
+			expect_sym("end");
+		}
+	} else
+	{
+		ordinal_type();
+	}
+	
+	epilogue();
+}
+
+void field_list(void)
+{
+	prologue("field_list");
+	
+	/* no else because fields can be empty; no need to error correct */
+	if(match_type(identifier))
+	{
+		/* 
+			this variable is a little weird, but exists to properly parse field list;
+			basically, case must be preceded by a semicolon, ONLY IF, previous identifier have been declared.
+			There is probably a better way to do this, but idk.
+		*/
+		char case_after = 0;
+		
+		expect_type(identifier);
+		
+		while(match_sym(","))
+		{
+			expect_sym(",");
+			expect_type(identifier);	
+		}
+		
+		expect_sym(":");
+		data_type();
+		
+		while(match_sym(";"))
+		{
+			expect_sym(";");
+			
+			/* end or ) or case must follow */
+			if(match_sym("end") || match_sym(")")) break;
+			
+			if(match_sym("case"))
+			{
+				case_after = 1;
+				break;
+			}
+			
+			expect_type(identifier);
+		
+			while(match_sym(","))
+			{
+				expect_sym(",");
+				expect_type(identifier);	
+			}
+			
+			expect_sym(":");
+			data_type();
+		}
+		
+		if(case_after)
+		{
+			expect_sym("case");
+			
+			/* need to add distinction between identifier and type identifier */
+			expect_type(identifier);
+			
+			if(match_sym(":"))
+			{
+				expect_sym(":");
+				expect_type(identifier);
+			}
+			
+			expect_sym("of");
+			
+			/* first instance, must repeat below */
+			
+			constant();
+			while(match_sym(","))
+			{
+				expect_sym(",");
+				constant();
+			}
+			
+			expect_sym(":");
+			expect_sym("(");
+			field_list();
+			expect_sym(")");
+			
+			/* second instance */
+			
+			while(match_sym(";"))
+			{	
+				expect_sym(";");
+
+				/* end or ) must follow a field list */
+				if(match_sym("end") || match_sym(")")) break;
+			
+				/* need to add distinction between identifier and type identifier */
+				
+				constant();
+				while(match_sym(","))
+				{
+					expect_sym(",");
+					constant();
+				}
+				
+				expect_sym(":");
+				expect_sym("(");
+				field_list();
+				expect_sym(")");
+			}
+		}
+	} else if(match_sym("case"))
+	{
+		expect_sym("case");
+		
+		/* need to add distinction between identifier and type identifier */
+		expect_type(identifier);
+		
+		if(match_sym(":"))
+		{
+			expect_sym(":");
+			expect_type(identifier);
+		}
+		
+		expect_sym("of");
+		
+		/* first instance, must repeat below */
+		
+		constant();
+		while(match_sym(","))
+		{
+			expect_sym(",");
+			constant();
+		}
+		
+		expect_sym(":");
+		expect_sym("(");
+		field_list();
+		expect_sym(")");
+		
+		/* second instance */
+		
+		while(match_sym(";"))
+		{	
+			expect_sym(";");
+
+			/* end or ) must follow a field list */
+			if(match_sym("end") || match_sym(")")) break;
+		
+			/* need to add distinction between identifier and type identifier */
+			
+			constant();
+			while(match_sym(","))
+			{
+				expect_sym(",");
+				constant();
+			}
+			
+			expect_sym(":");
+			expect_sym("(");
+			field_list();
+			expect_sym(")");
+		}
+	}
 	
 	epilogue();
 }
@@ -395,15 +695,15 @@ void block(void)
 		} while(match_type(identifier));
 	}
 	
-	if(match_sym("type_pasc"))
+	if(match_sym("type"))
 	{
-		expect_sym("type_pasc");
+		expect_sym("type");
 		
 		do
 		{
 			expect_type(identifier);
 			expect_sym("=");
-			type_pasc();
+			data_type();
 			expect_sym(";");
 		} while(match_type(identifier));
 	}
@@ -424,7 +724,7 @@ void block(void)
 			
 			expect_sym(":");
 			
-			type_pasc();
+			data_type();
 			
 			expect_sym(";");
 		} while(match_type(identifier));
--- parser.h
+++ parser.h
@@ -1,18 +1,17 @@
 
-/*
-
+void constant(void);
+void variable(void);
 void factor(void);
 void term(void);
 void simple_expression(void);
 void expression(void);
-
-*/
-
-void constant(void);
+void write_param_list(void);
 void index_type_spec(void);
 void conformant_arr(void);
 void formal_param_list(void);
 void proc_or_func_head(void);
-void type_pasc(void);
+void ordinal_type(void);
+void data_type(void);
+void field_list(void);
 void block(void);
 void program(void);
